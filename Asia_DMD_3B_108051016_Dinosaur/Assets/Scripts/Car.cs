850aa220af0fd988bf565867e704aea9d315755a
ATsau <108051016@live.asia.edu.tw>
2021-10-23 00:10:20 +0800

    2021/10/23

---
+++ Asia_DMD_3B_108051016_Dinosaur/Assets/Scripts/Car.cs
@@ -0,0 +1,11 @@
+using UnityEngine;      //引用 Unity 引擎命名空間 : 可以使用此空間的 API　　    
+
+// 單行註解
+// ATSAU 2021
+// class 類別 : 一個物件的藍圖
+// 語法 : 類別 類別名稱{ 類別內容 (類別成員) }
+// 此類別要在 Unity 內使用必須繼承 MonoBehaviour
+
+ public class Car : MonoBehaviour 
+{
+}
