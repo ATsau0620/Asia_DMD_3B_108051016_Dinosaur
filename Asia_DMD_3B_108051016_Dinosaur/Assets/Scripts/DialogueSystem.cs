9a928cf6b7832e9554fa7e6a60a190376300ce28
ATsau0620 <91519801+ATsau0620@users.noreply.github.com>
2021-12-17 16:50:18 +0800

    20211217 part.2

--- Asia_DMD_3B_108051016_Dinosaur/Assets/Scripts/DialogueSystem.cs
+++ Asia_DMD_3B_108051016_Dinosaur/Assets/Scripts/DialogueSystem.cs
@@ -58,4 +58,23 @@ public class DialogueSystem : MonoBehaviour
         goDialogue.SetActive(false);
     }
 
+
+    /// <summary>
+    /// 開始對話
+    /// </summary>
+    /// <param name="contents">要顯示打字效果的對話內容</param>
+    public void StartDialogue(string[] contents) 
+    {
+        StartCoroutine(TypeEffect(contents));
+    }
+
+
+    /// <summary>
+    ///停止對話 
+    /// </summary>
+    public void StopDialogue()
+    {
+        StopAllCoroutines();             //停止協程
+        goDialogue.SetActive(false);     //隱藏對話介面       
+    }
 }
--- Asia_DMD_3B_108051016_Dinosaur/Assets/Scripts/NPC.cs
+++ Asia_DMD_3B_108051016_Dinosaur/Assets/Scripts/NPC.cs
@@ -11,5 +11,29 @@ public class NPC : MonoBehaviour
     public DataDialogue dataDialogue;
     [Header("對話系統")]
     public DialogueSystem dialogueSystem;
+    [Header("觸發對話的對象")]
+    public string target = "小恐龍";
 
+    //觸發開始事件
+    // 1.兩個物件都要有Collider 2D
+    // 2.兩個物件要有一個有 Rigibody 2D
+    // 3.兩個物件要有一個勾 Is Trigger 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == target)
+        {
+            dialogueSystem.StartDialogue(dataDialogue.dialogues);
+
+        }
+     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == target)
+        {
+            dialogueSystem.StopDialogue();
+        }
+       
+
+    }
 }
