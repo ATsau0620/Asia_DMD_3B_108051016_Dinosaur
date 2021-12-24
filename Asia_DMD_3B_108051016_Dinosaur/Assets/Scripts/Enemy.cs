d5c74a662d26cdc39e0ba84ede8790ce29760968
ATsau0620 <91519801+ATsau0620@users.noreply.github.com>
2021-12-24 14:21:50 +0800

    20211224 part.1

--- Asia_DMD_3B_108051016_Dinosaur/Assets/Scripts/Enemy.cs
+++ Asia_DMD_3B_108051016_Dinosaur/Assets/Scripts/Enemy.cs
@@ -12,6 +12,38 @@ public class Enemy : MonoBehaviour
     [Header("目標圖層")]
     public LayerMask layerTarget;
 
+    #endregion
+
+    #region 事件
+
+
+    private void OnDrawGizmos()
+    {
+        //指定圖示顏色
+        Gizmos.color = new Color(1, 0, 0, 0.3f);
+        //繪製立方體(中心、尺寸)
+        Gizmos.DrawCube(transform.position + transform.TransformDirection(v3TrackOffset), v3TrackSize);
+    }
+
+    private void Update()
+    {
+        CheckTaragetInArea();
+    }
 
     #endregion
+
+    #region 方法
+    /// <summary>
+    /// 檢查目標是否在區域內
+    /// </summary>
+    private void CheckTaragetInArea() 
+    {
+        //2D 物理.覆蓋盒型(中心，尺寸，角度)
+        Collider2D hit = Physics2D.OverlapBox(transform.position + transform.TransformDirection(v3TrackOffset), v3TrackSize, 0, layerTarget);
+
+        if (hit) print(hit.name);
+    }
+
+    #endregion
+
 }
